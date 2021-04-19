cc04a01b437e5ab783e57a06e05114fac108347a
mlhay <aycckmlh@gmail.com>
2021-04-19 15:17:11 +0300

    errormessages

---
+++ app/src/main/java/com/example/alertmessages/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.alertmessages;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Naber Lan Düdük",Toast.LENGTH_LONG).show(); //Uygulama Başlangıcında mesaj çıkartır görünür ve kapanır.
+    }
+    public void save(View view) {
+        AlertDialog.Builder alert=new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are you Sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+    Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_LONG).show();
+
+            }
+        });
+alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        Toast.makeText(MainActivity.this,"Not Saved",Toast.LENGTH_LONG).show();
+
+    }
+});
+alert.show();
+
+    }
+}
